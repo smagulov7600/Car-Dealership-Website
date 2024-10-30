01e815a5e1d342a1f296ac27766b426e0d24f15c
smagulov7600 <alikhansmagulov55@gmail.com>
2024-10-31 00:53:12 +0500

    Added functionality for menu-container

--- Source/functionality.js
+++ Source/functionality.js
@@ -1,7 +1,8 @@
-const menuBtn = document.querySelector(".nav-menu-button")
+const menuContainer = document.querySelector(".menu-container")
+const menuBtn = document.querySelector(".menu-button")
 const slideBar = document.querySelector(".slide")
 
-menuBtn.addEventListener("click", () => {
+menuContainer.addEventListener("click", () => {
   menuBtn.classList.toggle("open");
   slideBar.classList.toggle("open");
   document.body.classList.toggle("no-scroll");
