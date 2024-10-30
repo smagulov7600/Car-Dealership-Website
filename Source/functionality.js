22fa33850c98a40b32ed9dbb84b6352a8b88cedf
smagulov7600 <alikhansmagulov55@gmail.com>
2024-10-30 18:24:15 +0500

    Removed y-overflow for the sidebar menu

--- Source/functionality.js
+++ Source/functionality.js
@@ -1,7 +1,8 @@
-const menuBtn = document.querySelector(".nav-menu-button");
+const menuBtn = document.querySelector(".nav-menu-button")
 const slideBar = document.querySelector(".slide")
 
 menuBtn.addEventListener("click", () => {
   menuBtn.classList.toggle("open");
   slideBar.classList.toggle("open");
+  document.body.classList.toggle("no-scroll");
 });
