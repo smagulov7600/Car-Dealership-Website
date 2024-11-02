ae0a7c9153699be51a9287a40cbe6b71915ead70
smagulov7600 <alikhansmagulov55@gmail.com>
2024-11-02 21:41:07 +0500

    bleh

--- Source/functionality.js
+++
@@ -1,9 +0,0 @@
-const menuContainer = document.querySelector(".menu-container")
-const menuBtn = document.querySelector(".menu-button")
-const slideBar = document.querySelector(".slide")
-
-menuContainer.addEventListener("click", () => {
-  menuBtn.classList.toggle("open");
-  slideBar.classList.toggle("open");
-  document.body.classList.toggle("no-scroll");
-});
