d5c8fcc87a2e44226dae57eada109cd01fc4c758
smagulov7600 <alikhansmagulov55@gmail.com>
2024-11-05 19:42:04 +0500

    Added smooth transitions

--- Source/animations.js
+++ Source/animations.js
@@ -1,6 +1,6 @@
-function wait(ms) {
-  return new Promise((resolve) => setTimeout(resolve, ms));
-}
+const headerText = document.querySelector(".htext");
+const paragraphText = document.querySelector(".ptext");
+const exploreButton = document.querySelector(".explore-button");
 
 function changeMenu(slide, button, textclass) {
   document.querySelector(`.${slide}`).classList.toggle("open");
@@ -15,24 +15,37 @@ function changeMenu(slide, button, textclass) {
   }
 }
 
-window.addEventListener("load", () => {
-  document.querySelector(".htext").classList.add("active-text");
-  document.querySelector(".ptext").classList.add("active-text");
-  document.querySelector(".explore-button").classList.add("active-text");
-});
+const videoElement = document.getElementById("video");
+const videoSources = [
+  "/Videos/VideoTemplate1.mp4",
+  "/Videos/VideoTemplate2.mp4",
+  "/Videos/VideoTemplate3.mp4",
+];
+const headerTexts = ["Skylines", "Supra", "Nissan"];
+const paragraphTexts = ["GTR II Series", "A90 Release", "Z Series"];
 
+let letAnimationPlay = true;
+let currentCircle = document.getElementById("1");
+let currentVideoIndex = 0;
 
+function circlingAnimation(pathElement) {
+  pathElement.classList.remove("stopped");
+  pathElement.style.strokeDashoffset = "-97px";
 
-
-
-async function circlingAnimation(pathElement) {
-  setTimeout(() => {
-    pathElement.style.strokeDashoffset = "-97px";
-  }, 200);
-
-  pathElement.addEventListener("transitionend", () => {
-    changeVideo(currentCircle.id);
-  });
+  pathElement.addEventListener(
+    "transitionend",
+    async () => {
+      if (currentCircle.id == "1") {
+        changeVideo("2");
+      } else if (currentCircle.id == "2") {
+        changeVideo("3");
+      } else if (currentCircle.id == "3") {
+        pathElement.classList.add("stopped");
+        letAnimationPlay = false;
+      }
+    },
+    { once: true }
+  );
 }
 
 function stopAnimation(pathElement) {
@@ -40,33 +53,45 @@ function stopAnimation(pathElement) {
   pathElement.style.strokeDashoffset = "-48px";
 }
 
-let currentCircle = document.getElementById("1");
-
-document.addEventListener("DOMContentLoaded", () => {
-  circlingAnimation(
-    currentCircle.parentElement.lastElementChild.firstElementChild
-  );
-});
-
-const videoElement = document.getElementById("video");
-const videoSources = [
-  "/Videos/VideoTemplate1.mp4",
-  "/Videos/VideoTemplate2.mp4",
-  "/Videos/VideoTemplate3.mp4",
-];
-let currentVideoIndex = 0;
-
-function changePlayingVideo() {
+async function textAnimation(newId, newCircle) {
+  currentVideoIndex = parseInt(newId) - 1;
+  currentCircle = newCircle;
   const newVideoSource = videoSources[currentVideoIndex];
 
-  videoElement.classList.add("fade-out");
+  headerText.classList.remove("active-text");
+  paragraphText.classList.remove("active-text");
+  exploreButton.classList.remove("active-text");
 
-  setTimeout(() => {
-    videoElement.src = newVideoSource;
-    videoElement.load();
-    videoElement.play();
-    videoElement.classList.remove("fade-out");
-  }, 1000);
+  headerText.classList.add("inactive-text");
+  paragraphText.classList.add("inactive-text");
+  exploreButton.classList.add("inactive-text");
+
+  videoElement.style.opacity = 0;
+
+  headerText.addEventListener(
+    "animationend",
+    () => {
+      videoElement.style.opacity = 1;
+      headerText.textContent = headerTexts[currentVideoIndex];
+      paragraphText.textContent = paragraphTexts[currentVideoIndex];
+      videoElement.src = newVideoSource;
+      videoElement.load();
+
+      newCircle.classList.add("selected");
+      newCircle.parentElement.lastElementChild.classList.add("default-transform");
+
+      headerText.classList.remove("inactive-text");
+      paragraphText.classList.remove("inactive-text");
+      exploreButton.classList.remove("inactive-text");
+
+      headerText.classList.add("active-text");
+      paragraphText.classList.add("active-text");
+      exploreButton.classList.add("active-text");
+
+      videoElement.play();
+    },
+    { once: true }
+  );
 }
 
 function changeVideo(newId) {
@@ -75,20 +100,35 @@ function changeVideo(newId) {
     return;
   }
 
-  currentCircle.classList.remove("selected");
-  newCircle.classList.add("selected");
-
   const currentPathElement =
     currentCircle.parentElement.lastElementChild.firstElementChild;
   const newPathElement =
     newCircle.parentElement.lastElementChild.firstElementChild;
-  stopAnimation(currentPathElement);
-  circlingAnimation(newPathElement);
-  newPathElement.classList.remove("stopped");
-  currentCircle = newCircle;
 
-  currentVideoIndex = parseInt(newId) - 1;
-  changePlayingVideo();
+  currentCircle.classList.remove("selected");
+  currentPathElement.classList.remove("default-transform");
+
+  if (letAnimationPlay) {
+    stopAnimation(currentPathElement);
+    circlingAnimation(newPathElement);
+  } else {
+    currentPathElement.style.strokeDashoffset = "-48px";
+    newPathElement.style.strokeDashoffset = "-97px";
+  }
+
+  textAnimation(newId, newCircle);
 }
 
-videoElement.play();
+document.addEventListener("DOMContentLoaded", () => {
+  const pathElement = currentCircle.parentElement.lastElementChild;
+
+  headerText.classList.add("active-text");
+  paragraphText.classList.add("active-text");
+  exploreButton.classList.add("active-text");
+  currentCircle.classList.add("selected");
+  pathElement.classList.toggle("default-transform");
+  videoElement.style.opacity = 1;
+
+  circlingAnimation(pathElement.firstElementChild);
+  videoElement.play();
+});
